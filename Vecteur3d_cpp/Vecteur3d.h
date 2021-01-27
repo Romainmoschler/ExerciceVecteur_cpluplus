889dcb0a6a9e25120e4da60231d4529748f9d301
Romainmoschler <44899582+Romainmoschler@users.noreply.github.com>
2021-01-27 16:21:52 +0100

    fin Exercice 1

--- Vecteur3d_cpp/Vecteur3d.h
+++ Vecteur3d_cpp/Vecteur3d.h
@@ -25,16 +25,12 @@ public:
 
 	void Somme(vecteur3d vect2)
 	{
-		this->m_x += vect2.m_x;
-		this->m_y += vect2.m_y;
-		this->m_z += vect2.m_z;
+		*this = *this + vect2;
 	}
 
 	void Soustrait(vecteur3d vect2)
 	{
-		this->m_x -= vect2.m_x;
-		this->m_y -= vect2.m_y;
-		this->m_z -= vect2.m_z;
+		*this = *this - vect2;
 	}
 
 	void Multipli(vecteur3d vect2)
@@ -46,7 +42,7 @@ public:
 
 	int coincide(vecteur3d vect2)
 	{
-		if (this->m_x == vect2.m_x && this->m_y == vect2.m_y && this->m_z == vect2.m_z)
+		if (this == vect2)
 
 			return true;
 
@@ -58,7 +54,70 @@ public:
 
 	void affiche()
 	{
-		cout << "Coordonees: " << this->m_x << " " << this->m_y << " " << this->m_z << endl;
+		cout << "Coordonees: " << *this << endl;
+	}
+
+	int getX()
+	{
+		return this->m_x;
+	}
+
+	int getY()
+	{
+		return this->m_y;
+	}
+
+	int getZ()
+	{
+		return this->m_z;
+	}
+
+	friend vecteur3d operator+(vecteur3d vect1, vecteur3d vect2)
+	{
+		vecteur3d resultat;
+
+		resultat.m_x = vect1.m_x + vect2.m_x;
+		resultat.m_y = vect1.m_y + vect2.m_y;
+		resultat.m_z = vect1.m_z + vect2.m_z;
+
+		return resultat;
+	}
+
+	friend vecteur3d operator-(vecteur3d vect1, vecteur3d vect2)
+	{
+		vecteur3d resultat;
+
+		resultat.m_x = vect1.getX() - vect2.getX();
+		resultat.m_y = vect1.getY() - vect2.getY();
+		resultat.m_z = vect1.getZ() - vect2.getZ();
+
+		return resultat;
+	}
+
+	friend vecteur3d& operator==(const vecteur3d vect1,const vecteur3d vect2)
+	{
+		if (vect1.getX() == vect2.getX() && vect1.getY() == vect2.getY() && vect1.getZ() == vect2.getZ())
+
+			return true;
+
+		else
+
+			return false;
+	}
+
+	friend ostream& operator<<(ostream& os, vecteur3d& vect1) 
+	{
+			os << vect1.m_x << " " << vect1.m_y << " " << vect1.m_z;
+
+			return os;
+	}
+	
+	vecteur3d operator=(const vecteur3d vect1)
+	{
+		this->m_x = vect1.m_x;
+		this->m_y = vect1.m_y;
+		this->m_z = vect1.m_z;
+
+		return *this;
 	}
 };
-
--- Vecteur3d_cpp/main.cpp
+++ Vecteur3d_cpp/main.cpp
@@ -1,13 +1,17 @@
-
 #include <iostream>
+
 #include "Vecteur3d.h"
 
 using namespace std;
 
 int main()
 {
-	vecteur3d <float> vecteur1;
-	vecteur3d <int> vecteur2;
+	vecteur3d<float> vecteur1(5.005f, 7.25f, 25.17f);
+	vecteur3d<float> vecteur2(1.700f, 4.25f, 40.00f);
+
+	vecteur1.affiche();
+
+	vecteur1.Somme(vecteur2);
 
 	vecteur1.affiche();
 
