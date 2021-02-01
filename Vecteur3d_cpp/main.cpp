2f3857ed21ad8acb60f6f5d6f8d65cb821ae86ba
Romainmoschler <44899582+Romainmoschler@users.noreply.github.com>
2021-02-01 09:59:46 +0100

    vecteur2d

--- Vecteur3d_cpp/main.cpp
+++ Vecteur3d_cpp/main.cpp
@@ -9,12 +9,16 @@ int main()
 {
 	vecteur3d<float> vecteur1(5.005f, 7.25f, 25.17f);
 	vecteur3d<float> vecteur2(1.700f, 4.25f, 40.00f);
+	vecteur2d<float> vect1(1.f, 2.f);
+	vecteur2d<float> vect2(3.f, 4.f);
 
 	vecteur1.affiche();
 
 	vecteur1.Somme(vecteur2);
 
 	vecteur1.affiche();
+
+	vect2.affiche();
 
 	return 0;
 }
--- Vecteur3d_cpp/vecteur2d.h
+++ Vecteur3d_cpp/vecteur2d.h
@@ -9,7 +9,8 @@ using namespace std;
 template <class T>  class vecteur2d : vecteur3d <T>
 {
 public:
-	vecteur2d(T a = 0.0f, T b = 0.0f);
+	vecteur2d(T a = 0, T b = 0);
+
 
 	void Somme(vecteur2d vect2);
 
@@ -17,13 +18,21 @@ public:
 
 	void Multipli(vecteur2d vect2);
 
-	
+	bool coincide(vecteur2d vect1, vecteur2d vect2);
+
+	void affiche();
+
+	/*friend vecteur2d operator<<(ostream& os, vecteur3d& vect1)
+	{
+		os << vect1.getX() << " " << vect1.getY();
+		return os;
+	}*/
 
 
 };
 
 template<class T>
-inline vecteur2d<T>::vecteur2d(T a = 0.0f, T b = 0.0f)
+inline vecteur2d<T>::vecteur2d(T a , T b )
 {
 	this->m_x = a;
 	this->m_y = b;
@@ -50,3 +59,23 @@ inline void vecteur2d<T>::Multipli(vecteur2d vect2)
 	this->m_x = this->m_x * vect2.m_x;
 	this->m_y = this->m_y * vect2.m_y;
 }
+
+template <class T>
+inline bool vecteur2d<T>::coincide(vecteur2d vect1, vecteur2d vect2)
+{
+	if (vect1.m_x == vect2.m_x && vect1.m_y == vect2.m_y)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+template <class T>
+inline void vecteur2d<T>::affiche()
+{
+	cout << "Coordonees: " << this->m_x << " " << this->m_y << endl;
+}
+
